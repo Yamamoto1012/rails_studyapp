f70af8aefc5a2cd92cf536b8a9b787d6266c8ea0
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-04-10 17:16:46 +0900

    投稿一覧と、詳細画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
-  get 'posts/index'
+  resources :posts, only: [:index, :show]
+  get "posts/:id" => "posts#show"
   devise_for :users
 end
