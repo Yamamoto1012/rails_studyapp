d45980db20b2f5d4873650d8281bd1b0909bdd5f
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-04-19 00:06:13 +0900

    色々追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#index"
   resources :posts, only: [:index, :show]
   get "posts/:id" => "posts#show"
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
   devise_for :users
 end
