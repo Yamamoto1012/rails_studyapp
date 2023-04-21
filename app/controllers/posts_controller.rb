6e5d493696ae8dbfae7dfa765e2e52131a678399
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-04-21 15:50:44 +0900

    新規投稿可能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,18 @@ class PostsController < ApplicationController
     @id = params[:id]
     @post = Post.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content], user: current_user)
+
+    if @post.save
+      redirect_to("/posts")
+    else
+      flash[:alert] = 'Error'
+      redirect_to root_url
+    end
+  end
 end
